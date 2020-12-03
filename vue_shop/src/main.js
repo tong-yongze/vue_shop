30247acebdafa12252a859fb9815ef565cbf33cd
童永泽 <604232797@qq.com>
2020-12-03 16:05:09 +0800

    完成了分类参数的开发

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -6,6 +6,7 @@ import './assets/fonts/iconfont.css'
 
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 // 配置请求的根路径
@@ -29,6 +30,8 @@ import './plugins/element.js'
 Vue.use(ElementUI)  // 在全局把ElementUI 注册引用了
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
